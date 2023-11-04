a1ba72747f71db79bb4d810f8632c6a4ac27a9ce
qudusanimashaun <qanimashaun>
2023-11-04 17:21:31 +0000

    cleanups

--- main_test.ts
+++ main_test.ts
@@ -1,15 +1,15 @@
 // import { assertEquals } from "https://deno.land/std@0.204.0/assert/mod.ts";
 import { parse } from "./main.ts";
 
-// Deno.test(function parseAsosSingleProduct() {
+Deno.test(function parseAsosSingleProduct() {
 
-//     // get html text from test file
-//     const html = Deno.readTextFileSync("./test/test_files/asos_2.txt");
-//     const url = new URL("https://www.asos.com");
-//     const items = parse(html, url);
+    // get html text from test file
+    const html = Deno.readTextFileSync("./test/test_files/asos_2.txt");
+    const url = new URL("https://www.asos.com");
+    const items = parse(html, url);
 
-//     console.log(items);
-// });
+    console.log(items);
+});
 
 Deno.test(function parseAsosMixAndMatch() {
   // get html text from test file
@@ -17,5 +17,5 @@ Deno.test(function parseAsosMixAndMatch() {
   const url = new URL("https://www.asos.com");
   const items = parse(html, url);
 
-  // console.log(items);
+  console.log(items);
 });
--- parsers/asos_parser.ts
+++ parsers/asos_parser.ts
@@ -7,7 +7,6 @@ import {
   Domain,
   HTML,
   ID,
-  Image,
   Item,
   Price,
   Product,
